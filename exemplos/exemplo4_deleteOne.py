d5ddd0ef3497ccc4cc38986aae9042f8aa35179e
JonataMarino <jsmarino1993@outlook.com>
2022-04-12 12:05:35 -0300

    pyMongo

--- exemplos/exemplo4_deleteOne.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-#test git
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["Aula5DB"]
-mycol = mydb["customers"]
-
-myquery = {"address": "Mountain 21"}
-
-mycol.delete_one(myquery)
